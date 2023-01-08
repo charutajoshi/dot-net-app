2fd890f58e3b24fa1c242cbbd9641f37a975aa68
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-08 16:32:22 -0600

    Display query string value

--- MyFirstDotNetApp/Program.cs
+++ MyFirstDotNetApp/Program.cs
@@ -2,13 +2,19 @@
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["MyKey"] = "my value";
-    context.Response.Headers["Content-Type"] = "text/html"; 
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync("<h2>World</h2>"); 
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
 
-app.Run(); 
+app.Run();
+
+// Test with following URL - https://localhost:7011/?id=4
