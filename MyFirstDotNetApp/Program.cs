b8a8a663baa14fb738d7dd99b75c20d7fca02c09
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-08 16:10:29 -0600

    Initial commit

---
+++ MyFirstDotNetApp/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    context.Response.Headers["MyKey"] = "my value"; 
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync(" World"); 
+});
+
+app.Run(); 
